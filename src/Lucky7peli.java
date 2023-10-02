c177d79b768e4ecff27af1a640d77a4c77f81f71
Azoril <mikael.vatanen@gmail.com>
2023-10-02 21:50:13 +0300

    Added a loop
    Added a loop that ends when money ends

--- src/Lucky7peli.java
+++ src/Lucky7peli.java
@@ -10,35 +10,42 @@ public class Lucky7peli {
 		System.out.println("I want to play a game");
 		System.out.println("Name of the game is Lucky 7");
 		System.out.println("You will be given 3 dices with 10 sides that will be thrown and if one of them hits 7 you win");
-		
+		System.out.println("Each play costs you 1 dollar and each win gives you 3 dollars");
 		
 		Random r = new Random();	//random annetti r
+		Scanner scanner = new Scanner(System.in);
 		
 		int dice1, dice2, dice3;
+		String playAgain;
+		System.out.println("Enter money amount");
+		int money = scanner.nextInt();
 		
-		//nopille annetaan random numero 1-10
-		dice1 = r.nextInt(10) + 1;
-		dice2 = r.nextInt(10) + 1;
-		dice3 = r.nextInt(10) + 1;		
-		
-		
+
+
+		do//rahat lopppu peli loppuu
+		{
+			//nopille annetaan random numero 1-10 joka kerta uudestaan
+			dice1 = r.nextInt(10) + 1;
+			dice2 = r.nextInt(10) + 1;
+			dice3 = r.nextInt(10) + 1;
+			
 		if (dice1 == 7 | dice2 == 7 | dice3 == 7) //Jos jokin nopista on 7 toista
 			{
 				System.out.println("Congratz you win");
-				System.out.println("First dice was " + dice1);
-				System.out.println("Second dice was " + dice2);
-				System.out.println("Third dice was " + dice3);
-			}
+				System.out.println("Dice were " + dice1 + " " + dice2 + " " + dice3);
+				money += 3;
+			}//if loppu
 		
 		else //jos mikään noppa ei ollut 7 toista
 			{
 				System.out.println("You lost!");
-				System.out.println("Dice was " + dice1);
-				System.out.println("Second dice was " + dice2);
-				System.out.println("Third dice was " + dice3);
-			}
+				System.out.println("Dices were " + dice1 + " " + dice2 + " " + dice3);
+				money -= 1;
+			}//else loppu
 		
+		}		while (money >0);//while loppu
 		
+		System.out.println("Thanks for playing!");
 		
 		
 		
